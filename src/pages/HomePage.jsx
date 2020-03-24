68faff50d58e7c19476cf6ccb6eed2b6d9843c67
DavidPayet <davidpayet570@gmail.com>
2020-03-24 01:14:43 +0100

    remove carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import About from '../components/About';
 import Shop from '../components/Shop';
 import BackToTopBtn from '../components/BackToTopBtn';
-import SliderVideo from '../components/SliderVideo';
+import '../styles/HomePage.scss';
 
 const HomePage = () => {
   return (
-    <div id="up-page">
-      <SliderVideo />
+    <div className="HomePage" id="up-page">
+      <div className="vidUp">
+      <video autoPlay loop src="https://res.cloudinary.com/dq39faoo1/video/upload/v1585004145/Pexels_Videos_1409899_ua2ofj.mp4" alt="Drone Video" className="vid1" />
+      </div>
       <About />
+      
+      <div className="vidCenter">
+      <video autoPlay loop src="https://res.cloudinary.com/dq39faoo1/video/upload/v1585003863/video_drone_yt7ppf.mp4" alt="Drone Video" className="vid2" />
+      </div>
       <Shop />
       <BackToTopBtn />
     </div>
